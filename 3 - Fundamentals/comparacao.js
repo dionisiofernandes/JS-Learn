6c01e7eb59358a014f509625a9f9032c529d7723
Luís Dionísio Fernandes <56206352+dionisiofernandes@users.noreply.github.com>
2021-11-23 20:34:28 +0000

    20211123

---
+++ 3 - Fundamentals/comparacao.js
@@ -0,0 +1,21 @@
+
+
+// Operadores de comparação
+
+// Números
+console.log( 1 > 2); // false
+console.log( 1 < 2); // true
+
+// Podemos comparar strings (ordem alfabeto)
+console.log( 'a' < 'b'); // true
+
+// igualdade
+console.log(2 == '2'); console.log(2 == '2');
+// igualdade estrita (olha ao datatype)
+console.log(2 === '2'); // false
+// Não igualdade
+console.log(2 != '2'); // false
+console.log(2 !== '2'); // true
+// null and undefined
+console.log(null == undefined); // true
+console.log(null === undefined); // false
