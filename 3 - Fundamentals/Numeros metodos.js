22c669864f2f090f3f9136610534fb4fed2b2581
Luís Dionísio Fernandes <56206352+dionisiofernandes@users.noreply.github.com>
2021-11-23 20:09:28 +0000

    20211123

---
+++ 3 - Fundamentals/Numeros metodos.js
@@ -0,0 +1,50 @@
+
+
+const numero1 = 50;
+const numero2 = 40;
+const numero3 = -20;
+
+let resultado;
+
+// Adição
+resultado = numero1 + numero2;
+// Subtração
+resultado = numero1 - numero2;
+// Divisão
+resultado = numero1 / numero2;
+// Multiplicação
+resultado = numero1 * numero2;
+// Modulo
+resultado = numero1 % numero2;
+
+// Pi
+resultado = Math.PI;
+// Arredondar
+resultado = Math.round(2.2);
+// Arredondar para cima
+resultado = Math.ceil(2.4);
+// Arredondar para baixo
+resultado = Math.floor(2.4);
+// Raiz quadrada
+resultado = Math.sqrt(5);
+// Valor absoluto
+resultado = Math.abs(-5);
+// Potencia (neste caso oito ao quadrado)
+resultado = Math.pow(8, 2);
+// Maior numero de uma lista
+resultado = Math.max(1,2,3,4,5,6,10);
+// Menor numero de uma lista
+resultado = Math.min(1,2,3,4,5,6,10,-3);
+
+
+
+
+
+
+
+
+
+
+
+console.log(resultado)
+
